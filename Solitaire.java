ae7371c9cbf5c060b45b5872da7aaba0535b8120
realpatato <jason.t.richardii@gmail.com>
2025-11-29 12:44:55 -0500

    Method for drawing the scree somewhat functional, definately not perfect yet

--- Solitaire.java
+++ Solitaire.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 class Card {
     int val;
     String face, suit;
+    boolean isHidden;
 
     public Card(int v, String s) {
         val = v;
@@ -18,28 +19,77 @@ class Card {
             face = "K";
         }
         suit = s;
+        isHidden = false;
     }
 
-    public void printTop() {
-        System.out.println("O ------ O");
-        System.out.println("| " + face + suit +  "     |");
+    public String getFace() {
+        if (isHidden) {
+            return "? ";
+        } 
+        return face + " ";
     }
 
-    public void printBottom() {
-        System.out.println("|        |");
-        System.out.println("|        |");
-        System.out.println("|        |");
-        System.out.println("|     " + face + suit + " |");
-        System.out.println("O ------ O");
+    public String getSuit() {
+        if (isHidden) {
+            return "? ";
+        }
+        return suit + " ";
+    }
+
+    public void topToScreen(int r, int c, Screen s) {
+        if (r == 0) {
+            s.lines[0] += "     " + c + "         ";
+        }
+        s.lines[r * 2 + 1] += "O ------- O    ";
+        s.lines[r * 2 + 2] += "| " + getFace() + getSuit() + "    | " + (r+1) + " ";
+        if (r < 10) {
+            s.lines[r * 2 + 2] += " ";
+        }
+    }
+
+    public void bottomToScreen(int r, Screen s) {
+        s.lines[r * 2 + 3] += "|         |    ";
+        s.lines[r * 2 + 4] += "|         |    ";
+        s.lines[r * 2 + 5] += "|         |    ";
+        s.lines[r * 2 + 6] += "|     " + getFace() + getSuit() +"|    ";
+        s.lines[r * 2 + 7] += "O ------- O    ";
+    }
+}
+
+class Screen {
+    String[] lines;
+
+    public Screen() {
+        //This length is VERY specfic. This is the maximum amount of lines that could possibly be needed.
+        lines = new String[38];
+        //Sets every value in the lines to be an empty string so the output looks good.
+        for (int i = 0; i < 38; i++) {
+            lines[i] = "";
+        }
+    }
+
+    public void outputScreen() {
+        for (int i = 0; i < 38; i++) {
+            if (lines[i].equals("") == false) {
+                System.out.println(lines[i]);
+                lines[i] = "";
+            }
+        }
     }
 }
 
 public class Solitaire {
     public static void main(String args[]) {
+        Screen screen = new Screen();
         Card card1 = new Card(11, "C");
-        Card card2 = new Card(13, "\u20AC");
-        card2.printTop();
-        card1.printTop();
-        card1.printBottom();
+        Card card2 = new Card(13, "D");
+        Card card3 = new Card(12, "S");
+        card3.topToScreen(0, 1, screen);
+        card2.topToScreen(0, 0, screen);
+        card1.topToScreen(1, 0, screen);
+        card1.bottomToScreen(1, screen);
+        card3.bottomToScreen(0, screen);
+        screen.outputScreen();
+        screen.outputScreen();
     }
 }
